a627a7d28951260b5ffdfa237ceff9ca5560dc47
sek0002 <sekkevin@gmail.com>
2022-07-25 12:24:50 +1000

    Fixed error connecting to BioMART for gene conversions

--- R/gene_conversions_human.R
+++ R/gene_conversions_human.R
@@ -15,8 +15,8 @@
 
 convertHumanGeneList <- function(x){
 
-  human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
-  mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+  human = useEnsembl("ensembl",dataset = "hsapiens_gene_ensembl",host = "https://dec2021.archive.ensembl.org/")
+  mouse = useEnsembl("ensembl",dataset = "mmusculus_gene_ensembl",host = "https://dec2021.archive.ensembl.org/")
   genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
   humanx <- unique(genesV2)
   return(humanx)
